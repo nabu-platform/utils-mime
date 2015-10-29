007d5b2dafea597e15dabe5fa37929ee2bb8c0c6
alex <alex@nabu.be>
2015-10-29 08:34:56 +0100

    Updated chunked readable to better handle asynchronous data

--- src/main/java/be/nabu/utils/mime/util/ChunkedReadableByteContainer.java
+++ src/main/java/be/nabu/utils/mime/util/ChunkedReadableByteContainer.java
@@ -1,10 +1,11 @@
 package be.nabu.utils.mime.util;
 
+import static be.nabu.utils.io.IOUtils.wrap;
+
 import java.io.IOException;
 import java.text.ParseException;
 
 import be.nabu.utils.io.IOUtils;
-import static be.nabu.utils.io.IOUtils.*;
 import be.nabu.utils.io.api.ByteBuffer;
 import be.nabu.utils.io.api.DelimitedCharContainer;
 import be.nabu.utils.io.api.LimitedReadableContainer;
@@ -29,6 +30,8 @@ public class ChunkedReadableByteContainer implements HeaderProvider {
 	
 	private Header [] trailingHeaders = new Header[0];
 	
+	private String partialChunkHeader;
+	
 	public ChunkedReadableByteContainer(ReadableContainer<ByteBuffer> parent) {
 		this.parent = parent;
 	}
@@ -45,19 +48,50 @@ public class ChunkedReadableByteContainer implements HeaderProvider {
 			if (chunk == null || chunk.remainingData() == 0) {
 				// the chunk should be followed by a proper CRLF
 				if (chunk != null) {
-					if (parent.read(wrap(single, false)) != 1)
-						throw new IOException("The chunk was not followed by a linefeed");
-					if (single[0] == '\r' && parent.read(wrap(single, false)) != 1)
+					long delimiterRead = parent.read(wrap(single, false));
+					// no data to read the chunk ending
+					if (delimiterRead == 0) {
+						break;
+					}
+					if (single[0] == '\r') {
+						delimiterRead = parent.read(wrap(single, false));
+					}
+					if (delimiterRead == 0) {
+						break;
+					}
+					if (single[0] != '\n') {
 						throw new IOException("The chunk was not followed by a linefeed but " + single[0]);
-					if (single[0] != '\n')
-						throw new IOException("The chunk was not followed by a linefeed but " + single[0]);
+					}
+					else {
+						chunk = null;
+					}
 				}
 				// we need to read bytes until we come to a linefeed which indicates the end of a chunk size
 				// read max 1000 characters
+				int maxLength = 1000;
+				if (partialChunkHeader != null) {
+					maxLength -= partialChunkHeader.length();
+				}
 				DelimitedCharContainer container = IOUtils.delimit(new ReadableStraightByteToCharContainer(IOUtils.limitReadable(parent, 1000)), "\n");
 				String content = IOUtils.toString(container);
-				if (!container.isDelimiterFound())
-					throw new IOException("Could not find linefeed that delimits the chunk descriptor in a reasonable amount of bytes (1000)");
+				if (!container.isDelimiterFound()) {
+					if ((1000 - maxLength) + content.length() >= 1000) {
+						throw new IOException("Could not find linefeed that delimits the chunk descriptor in a reasonable amount of bytes (1000), read: " + content);
+					}
+					else {
+						if (partialChunkHeader == null) {
+							partialChunkHeader = content;
+						}
+						else {
+							partialChunkHeader += content;
+						}
+						break;
+					}
+				}
+				else if (partialChunkHeader != null) {
+					content = partialChunkHeader + content;
+					partialChunkHeader = null;
+				}
 				// strip trailing "\r" which should be there if it is well-formed
 				if (content.endsWith("\r"))
 					content = content.substring(0, content.length() - 1);
@@ -73,13 +107,17 @@ public class ChunkedReadableByteContainer implements HeaderProvider {
 				else if (chunkSize > maxChunkSize) {
 					throw new IOException("The chunk " + chunkSize + " is too big, max " + maxChunkSize + " allowed");
 				}
+//				System.out.println("Starting new chunk with size: " + chunkSize);
 				chunk = IOUtils.limitReadable(parent, chunkSize);
 			}
 			long copied = chunk.read(target);
 			// chunk is done
-			if (copied <= 0) {
+			if (copied < 0) {
 				chunk = null;
-				continue;
+				break;
+			}
+			else if (copied == 0) {
+				break;
 			}
 			totalRead += copied;
 		}
