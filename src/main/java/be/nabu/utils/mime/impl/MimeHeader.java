9bf475ec2a1e6949eba7fbb746f807f5b7c83229
alex <alex@nabu.be>
2016-03-19 09:25:08 +0100

    Added equals/hashcode to mimeheaders

--- src/main/java/be/nabu/utils/mime/impl/MimeHeader.java
+++ src/main/java/be/nabu/utils/mime/impl/MimeHeader.java
@@ -191,4 +191,54 @@ public class MimeHeader implements ModifiableHeader {
 	public void setValue(String value) {
 		this.value = value;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		else if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		MimeHeader other = (MimeHeader) obj;
+		if (comments == null) {
+			if (other.comments != null) {
+				return false;
+			}
+		}
+		else if (!comments.equals(other.comments)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		}
+		else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (value == null) {
+			if (other.value != null) {
+				return false;
+			}
+		}
+		else if (!value.equals(other.value)) {
+			return false;
+		}
+		return true;
+	}
+
 }
